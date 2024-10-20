4f33da889e64f7a2658d73d81b0b16f2888c2e77
VanyaMatka <xzotow@yandex.ru>
2024-10-20 17:24:16 +0500

    Создан код для 1 и 2 задачи

---
+++ menu_z1z2/menu1/menu1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace menu1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutDeveloperMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Имя разработчика: Ваше Имя\n Почта: your.email@example.com", "О разработчике", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
